0446f84f543f0e3d21f754d4f11fe0961e424780
Anshuman <sharmaaanshumaan@gmail.com>
2024-01-14 00:57:43 +0530

    :label: global data types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+type WordType = {
+    word : string,
+    meaning : string,
+    options : string[];
+}
+
+interface StateType {
+    loading : boolean;
+    results : string[];
+    words : WordType[];
+    error?: string;
+}
