90f7b278733091c1ae6dfacf88bf0e525be8b6b5
Anshuman <sharmaaanshumaan@gmail.com>
2024-01-14 00:58:42 +0530

    :ambulance: fetching the required data types and data

---
+++ src/constants/data.ts
@@ -0,0 +1,18 @@
+interface LanguagesType {
+    name : string,
+    code : string
+}
+
+export const languages:LanguagesType[] = [{
+    name : "Hindi",
+    code : "hn"
+}, {
+    name : "Japanese",
+    code : "jp"
+}, {
+    name : "French",
+    code : "fr"
+}, {
+    name : "Spanish",
+    code : "sh"
+}]
