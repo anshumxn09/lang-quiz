8274c39a68e8a0999d9bd9df853dc9949bcb87a6
Anshuman <sharmaaanshumaan@gmail.com>
2024-01-20 14:51:14 +0530

    :package: Using microsoft API to fetch words

--- src/utils/fetch.words.ts
+++ src/utils/fetch.words.ts
@@ -1,5 +1,51 @@
-// import axios from 'axios';
+import axios from "axios";
+import { generate } from "random-words";
 
-// export const fetchAllWords = async () =>{ 
+export const fetchAllWords = async (params: string) => {
+  try {
+    const words = generate(8).map((i) => ({
+      Text: i,
+    }));
 
-// }
+    console.log(words);
+
+    const response  = await axios.post(
+      "https://microsoft-translator-text.p.rapidapi.com/translate",
+      words,
+      {
+        params: {
+          "to[0]": `${params}`,
+          "api-version": "3.0",
+          profanityAction: "NoAction",
+          textType: "plain",
+        },
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key":
+            "8ab815fbd2mshb9c2e5298a542b7p12a327jsn9638ebd0ac63",
+          "X-RapidAPI-Host": "microsoft-translator-text.p.rapidapi.com",
+        },
+      }
+    );
+
+    const receivedWords : FetchWordsType[] = response.data;
+
+    console.log(receivedWords);
+
+    const actualData: WordType[] = receivedWords.map((word, i) => {
+        const {translations} = word;
+        const meaning = translations[0].text;
+        return {
+            word : words[i].Text,
+            meaning,
+            options : ["Anshuman", "Seema", "Mein", "Aur"]
+        }
+    })
+
+    console.log(actualData);
+
+  } catch (error) {
+    console.log(error);
+    throw new Error("Some error")
+  }
+};
