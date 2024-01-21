e03b3da1d7eb8aab50f341d6354d17a12185ab0a
Anshuman <sharmaaanshumaan@gmail.com>
2024-01-21 12:04:39 +0530

    :globe_with_meridians: Options are fixed and state is managed

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,8 +1,35 @@
 // import { useState } from "react";
+import { useDispatch, useSelector } from 'react-redux';
 import '../styles/Quiz.css';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { SaveResult } from '../reducers/slice';
 
 const Quiz = () => {
-  // const [radioAns, setAnswer] = useState<string>("");
+  const  navigate = useNavigate();
+  const {words} = useSelector((state : {rootReducer : StateType})  => state.rootReducer);
+  const [radioAns, setAnswer] = useState<string>("");
+  const [result, setResults] = useState<string[]>([]);
+  const [count, setCount] = useState<number>(0);
+  const dispatch = useDispatch();
+
+  const nextHandler = () => {
+    if(count === 7){
+      navigate("/result");
+    }
+
+    setResults((val) => [...val, radioAns]);
+    setCount((val) => val + 1);
+    setAnswer("");
+
+    console.log(result);
+  }
+
+  useEffect(() => {
+    dispatch(SaveResult(result));
+  }, [dispatch, result]);
+
+  if(!words || words.length === 0) navigate("/");
 
   return (
     <div className="quizContainer">
@@ -10,15 +37,25 @@ const Quiz = () => {
       <p className='color-dg' style={{
         fontWeight : "bolder",
         fontSize : "40px"
-      }}>1 - Randoms Words</p>
+      }}>{count+1} - {words[count]?.meaning}</p>
       <p className='color-dg'>Meaning</p>
 
       <div className="radioControls">
-      <input type="radio" name="radio" value="Option"/>
-      <span className='color-dg'>Value</span>
+      {
+        words[count].options.map((i) => {
+          return (
+            <>
+              <input type="radio" name="radio" value={i} onChange={(e) => setAnswer(e.target.value)} />
+              <span style={{
+                fontSize : "20px"
+              }} className='color-dg'>{i}</span> <br />
+            </>
+          )
+        })
+      }
       </div>
 
-      <button className="lang" style={{marginTop : "10px", cursor : "pointer"}}>Next</button>
+      <button onClick={nextHandler} className="lang" style={{marginTop : "10px", cursor : "pointer"}}>Next</button>
     </div>
   )
 }
