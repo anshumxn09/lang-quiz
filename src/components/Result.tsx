fae6480b3fb4f98ffe777f20b85e38b424f964de
Anshuman <sharmaaanshumaan@gmail.com>
2024-01-21 12:05:12 +0530

    :ok_hand: Initialized Results Page To Show Resulyts

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,6 +1,32 @@
+import { useSelector } from "react-redux";
+
 const Result = () => {
+  const {words, results} = useSelector((state : {rootReducer : StateType})  => state.rootReducer);
+
   return (
-    <div>Result</div>
+    <div className="resultContainer">
+      <span>You got 5 out of 6 Right</span>
+
+      <div className="resultBlock">
+        <div className="words">
+          <h4 style={{"textAlign" : "center"}} >Words</h4>
+          {
+            words.map((val) => (
+              <p style={{textAlign: "center", padding : "5px"}}>{val.word}</p>
+            ))
+          }
+        </div>
+
+        <div className="results">
+          <h4 style={{"textAlign" : "center"}} >Answer</h4>
+          {
+            results.map((val) => (
+              <p style={{textAlign : "center", padding : "5px"}}>{val}</p>
+            ))
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
